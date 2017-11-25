ecd5db8a9ae2e2c9afcdba73e88c26d9f96a3d94
LUCIAHUO <luciahuo@sas.upenn.edu>
2017-11-24 17:06:40 -0800

    working zomato API

--- public/components/SearchYelp.jsx
+++ public/components/SearchYelp.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
+import zomato from 'zomato.js';
+import querystring from 'querystring';
+import axios from 'axios'
 
 export default class SearchYelp extends React.Component {
   constructor(props) {
     super();
-    this.search = this.search.bind(this);
+    this.zomato = new zomato(process.env.APIKey);
+    // get a token
+    this.submitForm = this.submitForm.bind(this);
+    this.cancel = this.cancel.bind(this);
+    $('#submit').click(this.submitForm);
   }
-
-  search() {
+  submitForm(e) {
+    e.preventDefault();
+    var keyword = $('#keyword').val();
+    if (!keyword) {
+      alert('keyword is required');
+    } else {
+      this.zomato
+      .search({
+        q: keyword,
+        count: 10
+      })
+      .then(function(data) {
+        console.log(data);
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+    }
+  }
+  cancel() {
 
   }
-
   render() {
+    const {handleSubmit} = this.props;
     return (
-      <div className="yelp">
+      <div className="searchResto">
         <form className="search_restaurant_form"
-          onSubmit={this.search}
+            id="mySearch"
+            onSubmit={this.submitForm}
           >
           <p className="form-title">
             Search Restaurants
           </p>
+          <label>
+            Keyword
+            <input type="text" id="keyword"/>
+          </label>
+          <div className="form-controls">
+            <button type="submit" id="submit">Search Restaurants</button>
+            <button onClick={this.cancel}>Cancel</button>
+          </div>
         </form>
       </div>
     );
